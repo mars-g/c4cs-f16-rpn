2b190112612546d452c3234f209b4b25c12811df
Marshall <mngordon@umich.edu>
2016-10-05 04:23:04 -0400

    workign test for multiply and divide

--- test_rpn.py
+++ test_rpn.py
@@ -12,4 +12,10 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate(" 1 2 3 +")
+	def test_multiply(self):
+		result = rpn.calculate("2 3 *")
+		self.assertEqual(6, result)
+	def test_divide(self):
+		result = rpn.calculate("8 2 /")
+		self.assertEqual(4, result)
 
