7cdb19816c8dee7b326d5ab73c3135fcf51216c0
Marshall <mngordon@umich.edu>
2016-10-05 04:40:34 -0400

    Final version of rpn.py

--- rpn.py
+++ rpn.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token == '+':
-			arg1 = stack.pop()
+		try:
+			token = int(token)
+			stack.append(token)
+		except ValueError:
+			function = operators[token]
 			arg2 = stack.pop()
-			result = arg1 + arg2
+			arg1 = stack.pop()
+			result = function(arg1,arg2)
 			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		elif token == '*':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 * arg2
-			stack.append(result)
-		elif token == '/':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 / arg2
-			stack.append(result) 
-
-		else:
-			stack.append(int(token))
 		print(stack)
+
+
 	if len(stack) != 1:
 		raise TypeError("Thats too much man!")
 	return stack.pop()
